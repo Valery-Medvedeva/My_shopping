229f6e1d6ed0bae1a2d24902977a1e4fca72866f
Valery Medvedeva <smerdovava@gmail.com>
2024-01-15 02:12:17 +0600

    Creating domain layer

---
+++ app/src/main/java/com/example/myshopping/domain/GetShopListUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.myshopping.domain
+
+class GetShopListUseCase(private val shopListRepository: ShopListRepository)  {
+    fun getShopList(): List<ShopItem>{
+        return shopListRepository.getShopList()
+    }
+}
--- app/src/main/java/com/example/myshopping/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myshopping/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.myshopping
+package com.example.myshopping.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myshopping.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
